46a16889794fb3000599d9f710a7342f87b8de27
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-11-01 00:19:11 +0700

    update key firebase

--- firebase.ts
+++ firebase.ts
@@ -7,7 +7,7 @@ import {getStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBOCGjZb_2ubFz5Lwrf73RryLnOswv6jRg",
+    apiKey: process.env.NEXT_PUBLIC_KEY_FIREBASE,
     authDomain: "nhu-my-studio.firebaseapp.com",
     projectId: "nhu-my-studio",
     storageBucket: "nhu-my-studio.appspot.com",
