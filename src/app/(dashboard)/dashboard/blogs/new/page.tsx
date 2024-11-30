be00a0d5b633c44f471ea1fc93cef8e4b9da1148
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-11-30 22:36:40 +0700

    update dynamic blogform new

--- src/app/(dashboard)/dashboard/blogs/new/page.tsx
+++ src/app/(dashboard)/dashboard/blogs/new/page.tsx
@@ -1,10 +1,14 @@
 "use client"
-import {BlogForm} from "@/components/dashboard/sections/blogs/create-update-form";
 
+import dynamic from 'next/dynamic';
+
+const BlogForm = dynamic(() => import('@/components/dashboard/sections/blogs/create-update-form').then((mod) => mod.BlogForm), {
+    ssr: false,  // Tắt SSR nếu cần thiết
+});
 export default function Page() {
     return (
         <div className="space-y-6">
-            <BlogForm initialData={null}/>
+            <BlogForm initialData={null} />
         </div>
     )
 }
