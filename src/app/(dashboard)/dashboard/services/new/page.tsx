dd651dc2dbf4768479c929828f0d5a4eb35ebf27
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-02 15:16:48 +0700

    update dynamic

--- src/app/(dashboard)/dashboard/services/new/page.tsx
+++ src/app/(dashboard)/dashboard/services/new/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
+import { ServiceForm } from "@/components/dashboard/sections/services/create-update-form";
 import dynamic from "next/dynamic";
 
-// Dùng dynamic để import ServiceForm một cách động
-const ServiceForm = dynamic(() => import('@/components/dashboard/sections/services/create-update-form').then((mod) => mod.ServiceForm), {
-    ssr: false,  // Tắt SSR nếu cần thiết
-});
 export default function Page() {
     return (
         <div className="space-y-6">
