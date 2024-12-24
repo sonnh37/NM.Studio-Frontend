d7f77ad9963c7b20c73314d480aace983b1eee32
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-24 14:12:31 +0700

    check middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,23 +26,25 @@ export function middleware(req: NextRequest) {
             return NextResponse.next();
         }
 
-        const decodedToken = decodeJwt(accessToken ?? "");
-        const {exp, Role} = decodedToken;
-        const currentTime = Math.floor(Date.now() / 1000);
+        if (accessToken && refreshToken) {
+            const decodedToken = decodeJwt(accessToken ?? "");
+            const {exp, Role} = decodedToken;
+            const currentTime = Math.floor(Date.now() / 1000);
 
-        if (exp && exp < currentTime) {
-            const url = new URL("/login", req.url);
-            url.searchParams.set("message", "Phiên đăng nhập đã hết hạn.");
-            return NextResponse.redirect(url);
-        }
+            if (exp && exp < currentTime) {
+                const url = new URL("/login", req.url);
+                url.searchParams.set("message", "Phiên đăng nhập đã hết hạn.");
+                return NextResponse.redirect(url);
+            }
 
-        if (
-            req.nextUrl.pathname.startsWith("/dashboard") &&
-            !["admin", "staff"].includes((Role as string)?.toLowerCase())
-        ) {
-            const url = new URL("/", req.url);
-            // url.searchParams.set("message", "Bạn không có quyền truy cập.");
-            return NextResponse.redirect(url);
+            if (
+                req.nextUrl.pathname.startsWith("/dashboard") &&
+                !["admin", "staff"].includes((Role as string)?.toLowerCase())
+            ) {
+                const url = new URL("/", req.url);
+                // url.searchParams.set("message", "Bạn không có quyền truy cập.");
+                return NextResponse.redirect(url);
+            }
         }
 
         return NextResponse.next();
