4c1bada3e626e9c11e03d2a20bc54666acca9058
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-24 14:30:13 +0700

    update trustHost next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  experimental: {
+    trustHost: true,
+  },
   images: {
     remotePatterns: [
       {
