6fdcc7ad6438f302f4303d962d8af61ef1a490f2
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-25 17:37:36 +0700

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,40 +7,40 @@ export function middleware(req: NextRequest) {
     console.log("All cookies:", req.cookies.getAll());
     const accessToken = req.cookies.get("accessToken")?.value;
 
-    if (accessToken) {
-      const decodedToken = decodeJwt(accessToken ?? "");
-      const { exp, Role } = decodedToken;
-      // const currentTime = Math.floor(Date.now() / 1000);
+    // if (accessToken) {
+    //   const decodedToken = decodeJwt(accessToken ?? "");
+    //   const { exp, Role } = decodedToken;
+    //   // const currentTime = Math.floor(Date.now() / 1000);
 
-      // check /login và role admin, staff, customer
-      if (req.nextUrl.pathname.startsWith("/login")) {
-        if (["admin", "staff"].includes((Role as string)?.toLowerCase())) {
-          const url = new URL("/dashboard", req.url);
-          return NextResponse.redirect(url);
-        }
+    //   // check /login và role admin, staff, customer
+    //   if (req.nextUrl.pathname.startsWith("/login")) {
+    //     if (["admin", "staff"].includes((Role as string)?.toLowerCase())) {
+    //       const url = new URL("/dashboard", req.url);
+    //       return NextResponse.redirect(url);
+    //     }
 
-        const url = new URL("/", req.url);
-        return NextResponse.redirect(url);
-      }
+    //     const url = new URL("/", req.url);
+    //     return NextResponse.redirect(url);
+    //   }
 
-      if (req.nextUrl.pathname.startsWith("/dashboard")) {
-        if (!["admin", "staff"].includes((Role as string)?.toLowerCase())) {
-          const url = new URL("/", req.url);
-          return NextResponse.redirect(url);
-        }
+    //   if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    //     if (!["admin", "staff"].includes((Role as string)?.toLowerCase())) {
+    //       const url = new URL("/", req.url);
+    //       return NextResponse.redirect(url);
+    //     }
 
-        return NextResponse.next();
-      }
+    //     return NextResponse.next();
+    //   }
 
-      return NextResponse.next();
-    } else {
-      if (req.nextUrl.pathname.startsWith("/dashboard")) {
-        const url = new URL("/", req.url);
-        return NextResponse.redirect(url);
-      }
+    //   return NextResponse.next();
+    // } else {
+    //   if (req.nextUrl.pathname.startsWith("/dashboard")) {
+    //     const url = new URL("/", req.url);
+    //     return NextResponse.redirect(url);
+    //   }
 
-      return NextResponse.next();
-    }
+    //   return NextResponse.next();
+    // }
   } catch (error) {
     console.error("Error in middleware:", error);
     const url = new URL("/error", req.url);
