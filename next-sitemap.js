27a55891994e65ffe914cc4b9d7f2292b814e89c
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-31 17:57:16 +0700

    update sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,16 @@
+// next-sitemap.js
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://yourdomain.com", // Thay 'yourdomain.com' bằng tên miền của bạn
+  generateRobotsTxt: true, // Tạo file robots.txt tự động
+  sitemapSize: 20,
+  transform: async (config, path) => {
+    // Kiểm tra nếu URL bắt đầu bằng /dashboard, trả về null để loại trừ
+    if (path.startsWith("/dashboard")) {
+      return null; // Loại bỏ trang này khỏi sitemap
+    }
+    return {
+      loc: path, // URL sẽ được bao gồm trong sitemap
+      lastmod: new Date().toISOString(), // Thời gian sửa đổi trang
+    };
+  },
+};
