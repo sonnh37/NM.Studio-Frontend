e3fbcb610dc3a74c35722466f57cf30c2e89aacf
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-31 20:59:07 +0700

    update add metadata with route /about

---
+++ src/app/(client)/about/metadata.ts
@@ -0,0 +1,7 @@
+// filepath: /d:/Projects/Personal/shadcn-editor-tiptap/NM.Studio-Frontend/src/app/(client)/about/metadata.ts
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'About Us - NhuMy Studio',
+  description: 'Learn more about NhuMy Studio - Studio áo dài, vest, đồ cưới.',
+};
