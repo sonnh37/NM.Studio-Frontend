c49fcfee9e944338ec38a610101907ccdbdbf3cc
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-31 21:42:31 +0700

    update new content about

--- src/app/(client)/about/page.tsx
+++ src/app/(client)/about/page.tsx
@@ -3,8 +3,8 @@ import AboutPage from "./about";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Về NhuMy Studio',
-  description: 'Tại NhuMyStudio makeup chuyên nghiệp - Studio áo dài, vest, đồ cưới.',
+  title: 'Dịch vụ Makeup & Áo Dài Cưới - Về NhuMy Studio',
+  description: 'Tại NhuMyStudio, chúng tôi cung cấp dịch vụ makeup chuyên sâu, áo dài cưới, vest, và trang phục sự kiện chất lượng cao. Khám phá ngay!',
   keywords: 'makeup chuyên nghiệp, studio áo dài, makeup như my, makeup chuyên sâu, studio vest, đồ cưới, trang điểm, áo dài cưới, vest cưới, dịch vụ makeup',
   robots: 'index, follow', // Yêu cầu Google lập chỉ mục và theo dõi các liên kết trong trang
 };
