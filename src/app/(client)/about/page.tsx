2ab68807e9c3a233e180b5d0d6ea03a2fc5224e3
Son Nguyen <nguyenson20zzzz@gmail.com>
2024-12-31 21:25:32 +0700

    fix metadata

--- src/app/(client)/about/page.tsx
+++ src/app/(client)/about/page.tsx
@@ -3,8 +3,10 @@ import AboutPage from "./about";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'About Us - NhuMy Studio',
+  title: 'Về NhuMy Studio',
   description: 'Tại NhuMyStudio makeup chuyên nghiệp - Studio áo dài, vest, đồ cưới.',
+  keywords: 'makeup chuyên nghiệp, studio áo dài, makeup như my, makeup chuyên sâu, studio vest, đồ cưới, trang điểm, áo dài cưới, vest cưới, dịch vụ makeup',
+  robots: 'index, follow', // Yêu cầu Google lập chỉ mục và theo dõi các liên kết trong trang
 };
 
 export default function Page() {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
           strategy="lazyOnload"
           crossOrigin="anonymous"
         />
-        <title>Studio</title>
       </head>
       <body suppressHydrationWarning={true}>
         <NextTopLoader
