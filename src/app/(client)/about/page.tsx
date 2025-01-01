cd2cab02dbf98aea722b35c10891184154bb9e65
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-01-01 08:08:18 +0700

    fix code

--- src/app/(client)/about/page.tsx
+++ src/app/(client)/about/page.tsx
@@ -1,20 +1,20 @@
-
 import type { Metadata } from "next";
 import Head from "next/head";
 import AboutPage from "./about";
 
 export const metadata: Metadata = {
-  title: 'Makeup Chuyên Sâu & Áo Dài Cưới Tại NhuMy Studio',
-  description: 'NhuMy Studio chuyên dịch vụ makeup chuyên sâu, áo dài cưới, vest, và trang phục sự kiện. Mang đến sự tinh tế, chuyên nghiệp và phong cách cá nhân hóa.',
-  keywords: 'makeup chuyên sâu, áo dài cưới, dịch vụ vest cưới, trang phục sự kiện, studio chuyên nghiệp',
-  robots: 'index, follow',
+  title: "Makeup Chuyên Sâu & Áo Dài Cưới Tại NhuMy Studio",
+  description:
+    "NhuMy Studio chuyên dịch vụ makeup chuyên sâu, áo dài cưới, vest, và trang phục sự kiện. Mang đến sự tinh tế, chuyên nghiệp và phong cách cá nhân hóa.",
+  keywords:
+    "makeup chuyên sâu, áo dài cưới, dịch vụ vest cưới, trang phục sự kiện, studio chuyên nghiệp",
+  robots: "index, follow",
 };
 
 export default function Page() {
-  return <>
-  <Head>
-    <link rel="canonical" href={`${process.env.NEXT_PUBLIC_SITE_URL}/about`} />
-  </Head>
-  <AboutPage />
-</>
+  return (
+    <>
+      <AboutPage />
+    </>
+  );
 }
