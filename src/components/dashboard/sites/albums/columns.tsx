e67d6c491d12b4faf80004d1f2524a8d29979c8f
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-01-08 13:44:44 +0700

    remove action album

--- src/components/dashboard/sites/albums/columns.tsx
+++ src/components/dashboard/sites/albums/columns.tsx
@@ -1,121 +1,191 @@
-"use client"
+"use client";
 
-import {ColumnDef} from "@tanstack/react-table"
-import {Album} from "@/types/album";
-import React from "react";
-import {Checkbox} from "@/components/ui/checkbox";
-import {DataTableColumnHeader} from "@/components/dashboard/common/data-table/data-table-column-header";
+import { DataTableColumnHeader } from "@/components/dashboard/common/data-table/data-table-column-header";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Album } from "@/types/album";
+import { ColumnDef, Row } from "@tanstack/react-table";
 import Image from "next/image";
 import Link from "next/link";
-import Actions from "./actions";
 
-
+import { DeleteBaseEntitysDialog } from "@/components/_common/data-table-generic/delete-dialog-generic";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { albumService } from "@/services/album-service";
+import { MoreHorizontal } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 export const columns: ColumnDef<Album>[] = [
-    {
-        accessorKey: "select",
-        header: ({table}) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({row}) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-    },
-    {
-        accessorKey: "actions",
-        header: "Actions",
-        cell: ({row}) => {
-            return <Actions row={row}/>;
-        },
-    },
-    {
-
-        accessorKey: "background",
-        header: "Background Image",
-        cell: ({row}) => {
-            const backgroundUrl = row.getValue("background") as string; // Lấy URL của hình ảnh từ dữ liệu album
-            return (
-                <Link href={backgroundUrl || "#"}>
-                    <Image
-                        alt={`Album background`}
-                        className="aspect-square rounded-md object-cover"
-                        height={64}
-                        width={64}
-                        src={backgroundUrl ?? ''} // Sử dụng đường dẫn hình ảnh từ dữ liệu
-                    />
-                </Link>
-            );
-        },
-    },
-    {
-        accessorKey: "title",
-        header: ({column}) => (
-            <DataTableColumnHeader column={column} title="Title"/>
-        )
-    },
-    {
-        accessorKey: "description",
-        header: ({column}) => (
-            <DataTableColumnHeader column={column} title="Description"/>
-        ),
-        cell: ({row}) => {
-            return <div className="truncate max-w-xs">{row.getValue("description")}</div>
+  {
+    accessorKey: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
         }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: ({ row }) => <Actions row={row} />,
+  },
+  {
+    accessorKey: "background",
+    header: "Background Image",
+    cell: ({ row }) => {
+      const backgroundUrl = row.getValue("background") as string; // Lấy URL của hình ảnh từ dữ liệu album
+      return (
+        <Link href={backgroundUrl || "#"}>
+          <Image
+            alt={`Album background`}
+            className="aspect-square rounded-md object-cover"
+            height={64}
+            width={64}
+            src={backgroundUrl ?? ""} // Sử dụng đường dẫn hình ảnh từ dữ liệu
+          />
+        </Link>
+      );
     },
-    {
-        accessorKey: "createdDate",
-        header: ({column}) => (
-            <DataTableColumnHeader column={column} title="Data created"/>
-        ),
-        cell: ({row}) => {
-            const date = new Date(row.getValue("createdDate"));
-            return date.toLocaleDateString('en-US', {
-                weekday: 'short', // Thu
-                year: 'numeric',  // 2022
-                month: 'short',   // Oct
-                day: 'numeric'    // 20
-            });
-        }
+  },
+  {
+    accessorKey: "title",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Title" />
+    ),
+  },
+  {
+    accessorKey: "description",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Description" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="truncate max-w-xs">{row.getValue("description")}</div>
+      );
     },
-    {
-        accessorKey: "isDeleted",
-        header: ({column}) => <DataTableColumnHeader column={column} title="Is Deleted"/>,
-        cell: ({row}) => {
-            const isDeleted = row.getValue("isDeleted") as boolean;
-            if (!isDeleted) {
-                return (
-                    <Image
-                        src="https://firebasestorage.googleapis.com/v0/b/smart-thrive.appspot.com/o/Blog%2Fcheck.png?alt=media&token=1bdb7751-4bdc-4af1-b6e1-9b758df3a3d5"
-                        width={500}
-                        height={500}
-                        alt="Gallery Icon"
-                        className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"
-                    />
-                );
-            }
-            return (
-                <Image
-                    src="https://firebasestorage.googleapis.com/v0/b/smart-thrive.appspot.com/o/Blog%2Funcheck.png?alt=media&token=3b2b94d3-1c59-4a96-b4c6-312033d868b1"
-                    width={500}
-                    height={500}
-                    alt="Gallery Icon"
-                    className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"
-                />
-            );
-        },
-        filterFn: (row, id, value) => {
-            return value.includes(row.getValue(id));
-        },
+  },
+  {
+    accessorKey: "createdDate",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Data created" />
+    ),
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("createdDate"));
+      return date.toLocaleDateString("en-US", {
+        weekday: "short", // Thu
+        year: "numeric", // 2022
+        month: "short", // Oct
+        day: "numeric", // 20
+      });
     },
+  },
+  {
+    accessorKey: "isDeleted",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Is Deleted" />
+    ),
+    cell: ({ row }) => {
+      const isDeleted = row.getValue("isDeleted") as boolean;
+      if (!isDeleted) {
+        return (
+          <Image
+            src="https://firebasestorage.googleapis.com/v0/b/smart-thrive.appspot.com/o/Blog%2Fcheck.png?alt=media&token=1bdb7751-4bdc-4af1-b6e1-9b758df3a3d5"
+            width={500}
+            height={500}
+            alt="Gallery Icon"
+            className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"
+          />
+        );
+      }
+      return (
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/smart-thrive.appspot.com/o/Blog%2Funcheck.png?alt=media&token=3b2b94d3-1c59-4a96-b4c6-312033d868b1"
+          width={500}
+          height={500}
+          alt="Gallery Icon"
+          className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0"
+        />
+      );
+    },
+    filterFn: (row, id, value) => {
+      return value.includes(row.getValue(id));
+    },
+  },
+];
 
-]
+interface ActionsProps {
+  row: Row<Album>;
+}
+
+const Actions: React.FC<ActionsProps> = ({ row }) => {
+  const model = row.original;
+  const router = useRouter();
+  const pathName = usePathname();
+  const handleEditClick = () => {
+    router.push(`${pathName}/${model.id}`);
+  };
+
+  const handleAlbumsClick = () => {
+    //row.toggleSelected();
+    router.push(`${pathName}?q=${model.id}`);
+  };
+
+  const [showDeleteTaskDialog, setShowDeleteTaskDialog] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem
+            onClick={() => navigator.clipboard.writeText(model.id!)}
+          >
+            Copy model ID
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleAlbumsClick}>
+            View photos
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleEditClick}>Edit</DropdownMenuItem>
+          <DropdownMenuItem onSelect={() => setShowDeleteTaskDialog(true)}>
+            Delete
+            <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteBaseEntitysDialog
+        deleteById={albumService.delete}
+        open={showDeleteTaskDialog}
+        onOpenChange={setShowDeleteTaskDialog}
+        list={[model]}
+        showTrigger={false}
+        onSuccess={() => row.toggleSelected(false)}
+      />
+    </>
+  );
+};
