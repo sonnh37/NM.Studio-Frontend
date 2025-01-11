f2698e812b0d80236c0429a5b173a0b4d73ce059
Son Nguyen <nguyenson20zzzz@gmail.com>
2025-01-11 21:32:40 +0700

    add new editor platejs

---
+++ src/components/editor/plate-editor.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
+import { Plate } from '@udecode/plate/react';
+
+import { useCreateEditor } from '@/components/editor/use-create-editor';
+import { SettingsDialog } from '@/components/editor/settings';
+import { Editor, EditorContainer } from '@/components/plate-ui/editor';
+const htmlValue = '<p>TAO là  <b>SƠN CA</b> and <i>italic</i> text!</p>';
+
+export function PlateEditor() {
+  const editor = useCreateEditor({value: htmlValue});
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Plate editor={editor}>
+        <EditorContainer>
+          <Editor variant="demo" />
+        </EditorContainer>
+
+        <SettingsDialog />
+      </Plate>
+    </DndProvider>
+  );
+}
